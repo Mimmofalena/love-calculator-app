ae4911132046ab2cd7d67193769f81cc62f0cb47
Domenico C <70837839+Mimmofalena@users.noreply.github.com>
2021-04-17 17:23:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,33 @@ const result = document.querySelector(".result");
 const inputFields = document.querySelectorAll("input");
 const resetButton = document.querySelector(".reset");
 
+const greatMatch = "This is love!! 😍";
+const goodMatch = "You are compatible, let's celebrate 🥂🎊";
+const averageMatch = "Your match is okay, but it could be definitely better ";
+const badMatch =
+  "There's plenty of fishes in the water.<br> Why not looking somewhere else instead?";
+
 const loverFunction = function () {
   const randomNumber = Math.random() * 100;
   return Math.floor(randomNumber) + 1;
 };
 
 let loverMatchRate = loverFunction();
+
+const getMatchScore = function (loveScore) {
+  if (loveScore > 90) {
+    return greatMatch;
+  }
+  if (loveScore > 70 && loveScore < 90) {
+    return goodMatch;
+  }
+  if (loveScore > 50 && loveScore < 70) {
+    return averageMatch;
+  }
+  if (loveScore < 50) {
+    return badMatch;
+  }
+};
 
 button.addEventListener("click", function (e) {
   e.preventDefault();
@@ -23,11 +44,10 @@ button.addEventListener("click", function (e) {
     return;
   }
 
-  console.log(secondName.value);
   result.classList.remove("result-hidden");
-  result.textContent = `💖Love Compatibility between ${firstName} and ${secondName} is ${loverMatchRate} %`;
-
-  //   inputFields.forEach((node) => (node.value = ""));
+  result.innerHTML = `💖Love Compatibility between ${firstName} and ${secondName} is ${loverMatchRate} % <br>  ${getMatchScore(
+    loverMatchRate
+  )}`;
 });
 
 resetButton.addEventListener("click", function () {
